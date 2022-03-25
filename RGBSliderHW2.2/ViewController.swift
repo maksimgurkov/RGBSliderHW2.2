3d7389a5dc0578618d2ed358967a84d97385b618
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-03-25 15:07:21 +0300

    Initial Commit

---
+++ RGBSliderHW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBSliderHW2.2
+//
+//  Created by Максим Гурков on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
